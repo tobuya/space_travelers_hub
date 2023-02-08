557f25ae49d9098d27a756277366fce345f4e080
tobuya <thomasobuya95@gmail.com>
2023-02-08 08:15:54 +0300

    Add missions props

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,24 +1,39 @@
 import { MDBBtn } from 'mdb-react-ui-kit';
+import PropTypes from 'prop-types';
 
-const Mission = () => (
-  <tr>
-    <td className="fw-bold align-top">Mission Name</td>
-    <td>
-      Mission Description Mission Description Mission Description Mission Description
-      Mission Description Mission Description Mission Description Mission Description
-      Mission Description Mission Description Mission Description Mission Description
-      Mission Description Mission Description Mission Description Mission Description
-      Mission Description Mission Description Mission Description Mission Description
-    </td>
-    <td>
-      <MDBBtn className="text-nowrap" color="dark" size="sm" disabled>NOT A MEMBER</MDBBtn>
-      <MDBBtn className="text-nowrap" size="sm" disabled>Active Member</MDBBtn>
-    </td>
-    <td>
-      <MDBBtn className="text-nowrap" outline color="dark">Join Mission</MDBBtn>
-      <MDBBtn className="text-nowrap" outline color="danger">Leave Mission</MDBBtn>
-    </td>
-  </tr>
-);
+const Mission = ({ mission }) => {
+  const {
+    name, description, membership,
+  } = mission;
+
+  return (
+    <tr>
+      <td className="fw-bold align-top">{name}</td>
+      <td>{description}</td>
+      <td>
+        {membership ? (
+          <MDBBtn className="text-nowrap" size="sm" disabled>Active Member</MDBBtn>
+        ) : (
+          <MDBBtn className="text-nowrap" color="dark" size="sm" disabled>NOT A MEMBER</MDBBtn>
+        )}
+      </td>
+      <td>
+        {membership ? (
+          <MDBBtn className="text-nowrap" outline color="danger">Leave Mission</MDBBtn>
+        ) : (
+          <MDBBtn className="text-nowrap" outline color="dark">Join Mission</MDBBtn>
+        )}
+      </td>
+    </tr>
+  );
+};
+
+Mission.propTypes = {
+  mission: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    membership: PropTypes.bool.isRequired,
+  }).isRequired,
+};
 
 export default Mission;
