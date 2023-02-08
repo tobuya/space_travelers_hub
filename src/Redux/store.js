86b14df4ba64dec6e6e0e6a38e5b6168218312fe
tobuya <thomasobuya95@gmail.com>
2023-02-08 08:14:22 +0300

    Add missions reducer to the store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './Rockets/Rockets';
+import missionsReducer from './Missions/missions';
 
 const store = configureStore({
   reducer: {
     rocket: rocketReducer,
+    missions: missionsReducer,
   },
 });
 
