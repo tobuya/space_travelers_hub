b2cade5a636ba533760a907e2052573df41cc183
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-02-08 15:41:39 +0300

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+import store from './Redux/store';
 import './App.css';
 import Navigation from './components/Navigation';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
-
-import Dragon from './components/Dragons';
-
 
 function App() {
   return (
@@ -16,9 +13,9 @@ function App() {
       <Provider store={store}>
         <Navigation />
         <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
           <Route />
-          <Route />
-          <Route path="/dragons" element={<Dragon />} />
           <Route />
         </Routes>
       </Provider>
