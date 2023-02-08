9d64a94f7b117a4248f9c5caf54e6881a290dd86
Outkarm <jkpordje@gmail.com>
2023-02-08 09:50:42 +0000

    fixing merg issue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './App.css';
 import Navigation from './components/Navigation';
+
 import Dragon from './components/Dragons';
 
 function App() {
   return (
     <Router>
-      <div>
+      <Provider store={store}>
         <Navigation />
         <Routes>
+          <Route />
+          <Route />
           <Route path="/dragons" element={<Dragon />} />
           <Route />
-          <Route />
-          <Route />
         </Routes>
-      </div>
+      </Provider>
     </Router>
   );
 }
