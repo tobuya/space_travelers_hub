96ca4d92b10b180f904690f93d5e897319e7cbcd
Outkarm <jkpordje@gmail.com>
2023-02-08 14:13:31 +0000

    fixing linters

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
