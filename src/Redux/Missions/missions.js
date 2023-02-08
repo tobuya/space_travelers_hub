0c054166089c0e54317472954f5a4ea10a3c640d
tobuya <thomasobuya95@gmail.com>
2023-02-08 21:36:36 +0300

    Implement joining, leaving and switching badges for missions

--- src/Redux/Missions/missions.js
+++ src/Redux/Missions/missions.js
@@ -5,6 +5,7 @@ const missionsAPI = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = [];
 const FETCHED_MISSIONS = 'space_travelers_hub/missions/FETCHED_MISSIONS';
+const TOGGLED_MISSION_STATE = 'space_travelers_hub/missions/TOGGLED_MISSIONS_STATE';
 
 const fetchMissions = createAsyncThunk(
   FETCHED_MISSIONS,
@@ -15,7 +16,7 @@ const fetchMissions = createAsyncThunk(
       name: mission.mission_name,
       id: mission.mission_id,
       description: mission.description,
-      membership: true,
+      reserved: false,
     }));
     return missions;
   },
@@ -25,10 +26,21 @@ const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case `${FETCHED_MISSIONS}/fulfilled`:
       return [...state, ...action.payload];
+    case TOGGLED_MISSION_STATE: {
+      return state.map((mission) => {
+        if (mission.id === action.payload) return { ...mission, reserved: !mission.reserved };
+        return mission;
+      });
+    }
     default:
       return state;
   }
 };
 
-export { fetchMissions };
+const toggleMissionReserveState = (id) => ({
+  type: TOGGLED_MISSION_STATE,
+  payload: id,
+});
+
+export { fetchMissions, toggleMissionReserveState };
 export default missionsReducer;
