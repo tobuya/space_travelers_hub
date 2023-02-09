352c3f479f87d3b75376a211ed25f6cecc5c1ec2
tobuya <thomasobuya95@gmail.com>
2023-02-09 14:40:09 +0300

    Render all joined missions on my profile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -5,7 +5,10 @@ import MyProfileRockets from '../components/MyProfile/MyProfileRockets';
 
 const MyProfile = () => {
   const dispatch = useDispatch();
+
   const rockets = useSelector((state) => state.rocket);
+  const missions = useSelector((state) => state.missions);
+
   useEffect(() => {
     if (rockets.length === 0) {
       dispatch(addedRockets());
@@ -13,6 +16,23 @@ const MyProfile = () => {
   }, [dispatch, rockets.length]);
   return (
     <>
+      <table className="table w-50 m-5">
+        <thead>
+          <tr>
+            <th><h3>My Missions</h3></th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions
+            .filter((mission) => mission.reserved)
+            .map((mission) => (
+              <tr key={mission.id} className="border">
+                <p className="m-2">{mission.name}</p>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
       <table className="table w-50 m-5">
         <thead>
           <tr>
