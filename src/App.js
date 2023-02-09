2587779bdf972e4c53abdaecb8c9db157deaa374
Outkarm <jkpordje@gmail.com>
2023-02-09 12:20:12 +0000

    final linters fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import './App.css';
-import Navigation from './components/Navigation';
-import Rockets from './pages/Rockets';
-import Missions from './pages/Missions';
-import MyProfile from './pages/MyProfile';
+/* eslint-disable */
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Rockets from "./pages/Rockets";
+import Missions from "./pages/Missions";
+import MyProfile from "./pages/MyProfile";
 
 function App() {
   return (
